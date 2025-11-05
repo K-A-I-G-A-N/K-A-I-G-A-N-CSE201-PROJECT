366d30d3b59cc3e057414554f2ca31fc7bbe5b6e
ander734 <ander734@miamioh.edu>
2025-11-05 11:20:34 -0500

    Add files via upload

---
+++ 201AppCatalogue/src/Admin/App_Approval_Form.java
@@ -0,0 +1,5 @@
+package Admin;
+
+public class App_Approval_Form {
+
+}
---
+++ 201AppCatalogue/src/Guest/Guest.java
@@ -0,0 +1,20 @@
+package Guest;
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class Guest {
+
+	public static void guestAccess() {
+
+		JFrame guest = new JFrame();
+		guest.setTitle("K A I G A N App_Catalogue");
+		guest.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		guest.setSize(1400,800);
+		guest.setResizable(false);
+		guest.setLocationRelativeTo(null);
+		guest.getContentPane().setBackground(new Color (255, 214, 178));
+		guest.setVisible(true);
+		
+	}
+
+}
